bca63cfdcc0d8a0e929a2b339953df1664eab03f
walker.zhang <walker5111@qq.com>
2017-04-09 16:55:16 +0800

    delete the files generated by sbe tool, and update the gitignore to include these files

--- ams/src/main/java/com/flying/ams/msg/gen/MetaAttribute.java
+++
@@ -1,9 +0,0 @@
-/* Generated SBE (Simple Binary Encoding) message codec */
-package com.flying.ams.msg.gen;
-
-public enum MetaAttribute
-{
-    EPOCH,
-    TIME_UNIT,
-    SEMANTIC_TYPE
-}
--- cms/src/main/java/com/flying/cms/msg/gen/MetaAttribute.java
+++
@@ -1,9 +0,0 @@
-/* Generated SBE (Simple Binary Encoding) message codec */
-package com.flying.cms.msg.gen;
-
-public enum MetaAttribute
-{
-    EPOCH,
-    TIME_UNIT,
-    SEMANTIC_TYPE
-}
